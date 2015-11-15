d974e891d906a83496d3c4ab60f0033773bc31a5
Raghu Betina <rbetina@users.noreply.github.com>
2015-11-15 08:25:28 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,73 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+usernames = ["alice", "bob", "carol"]
+
+usernames.each do |username|
+  user = User.create
+  user.username = username
+  user.email = "#{username}@example.com"
+  user.password = "12341234"
+  user.save
+end
+
+puts "There are now #{User.count} users in the database."
+
+photo_info = [
+  {
+    :image => "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Lake_Bondhus_Norway_2862.jpg/1280px-Lake_Bondhus_Norway_2862.jpg",
+    :caption => "Lake Bondhus"
+  },
+  {
+    :image => "http://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Lanzarote_5_Luc_Viatour.jpg/1280px-Lanzarote_5_Luc_Viatour.jpg",
+    :caption => "Cueva de los Verdes"
+  },
+  {
+    :image => "http://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Fire_breathing_2_Luc_Viatour.jpg/1280px-Fire_breathing_2_Luc_Viatour.jpg",
+    :caption => "Jaipur"
+  },
+  {
+    :image => "http://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Ніжний_ранковий_світло.jpg/1280px-Ніжний_ранковий_світло.jpg",
+    :caption => "Sviati Hory"
+  },
+  {
+    :image => "http://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Mostar_Old_Town_Panorama_2007.jpg/1280px-Mostar_Old_Town_Panorama_2007.jpg",
+    :caption => "Mostar"
+  },
+  {
+    :image => "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Elakala_Waterfalls_Swirling_Pool_Mossy_Rocks.jpg/1280px-Elakala_Waterfalls_Swirling_Pool_Mossy_Rocks.jpg",
+    :caption => "Elakala"
+  },
+  {
+    :image => "http://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Biandintz_eta_zaldiak_-_modified2.jpg/1280px-Biandintz_eta_zaldiak_-_modified2.jpg",
+    :caption => "Biandintz"
+  }
+]
+
+users = User.all
+
+users.each do |user|
+  user.photos.create photo_info
+end
+
+puts "There are now #{Photo.count} photos in the database."
+
+photos = Photo.all
+
+photos.each do |photo|
+  rand(6).times do
+    comment = photo.comments.build
+    comment.user = users.sample
+    comment.body = Faker::Hacker.say_something_smart
+    comment.save
+  end
+end
+
+puts "There are now #{Comment.count} comments in the database."
+
+photos.each do |photo|
+  users.sample(rand(users.count)).each do |user|
+    like = photo.likes.build
+    like.user = user
+    like.save
+  end
+end
+
+puts "There are now #{Like.count} likes in the database."
