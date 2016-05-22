8b4b899629ab99d22ae21c7907ed929950543a64
Mitch Emerson <mitchemerson@users.noreply.github.com>
2016-05-22 17:26:02 -0500

    create users#index, add link to nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root "photos#index"
 
+
+  get "/users", :controller => "users", :action => "index"
+
   # Routes for the Comment resource:
   # CREATE
   get "/comments/new", :controller => "comments", :action => "new"
