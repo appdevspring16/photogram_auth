2a4f6ab98ca8c0723c06ef58c48bfa0cb05dd78f
Mitch Emerson <mitchemerson@users.noreply.github.com>
2016-05-22 16:56:06 -0500

    comment validation fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,6 @@ class Comment < ActiveRecord::Base
 
   validates :user_id, :presence => true
   validates :photo_id, :presence => true
-  validates :body_id, :presence => true
+  validates :body, :presence => true
 
 end
