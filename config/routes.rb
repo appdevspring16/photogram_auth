2b471746faa09f7aeeb61bc28565147463289930
Mitch Emerson <mitchemerson@users.noreply.github.com>
2016-05-22 16:12:15 -0500

    generate like table starter

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Like resource:
+  # CREATE
+  get "/likes/new", :controller => "likes", :action => "new"
+  post "/create_like", :controller => "likes", :action => "create"
+
+  # READ
+  get "/likes", :controller => "likes", :action => "index"
+  get "/likes/:id", :controller => "likes", :action => "show"
+
+  # UPDATE
+  get "/likes/:id/edit", :controller => "likes", :action => "edit"
+  post "/update_like/:id", :controller => "likes", :action => "update"
+
+  # DELETE
+  get "/delete_like/:id", :controller => "likes", :action => "destroy"
+  #------------------------------
+
   # Routes for the Photo resource:
   # CREATE
   get "/photos/new", :controller => "photos", :action => "new"
