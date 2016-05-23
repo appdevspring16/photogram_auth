6b168aa76324d3cb245b13553db7fd3df0b678a4
Raghu Betina <rbetina@gmail.com>
2016-05-23 12:51:55 -0500

    Fix factories

--- spec/factories/like.rb
+++ spec/factories/like.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :like do
-    user_id 200
-    photo_id 1000
+    user
+    photo
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    username "alice"
+    email "alice@example.com"
+    password "123456"
   end
 end
