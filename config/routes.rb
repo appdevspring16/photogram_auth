e806f9f63ee7c1adf10939000d91260197ec4a23
Starr Marcello <starr.marcello@users.noreply.github.com>
2016-05-23 00:45:39 -0500

    stopping point 1

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,9 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
+
+  get "/users", :controller => "users", :action => "index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
