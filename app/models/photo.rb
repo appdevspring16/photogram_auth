ac5b78fcbe356d90dd1ddd5f5e6bfbe3adac44ed
meghankinney <meghankinney@users.noreply.github.com>
2016-05-23 00:04:43 +0000

    setting up

---
+++ app/models/photo.rb
@@ -0,0 +1,10 @@
+class Photo < ActiveRecord::Base
+
+  validates :user_id, :presence => true
+
+  has_many :comments
+  has_many :likes
+  has_many :fans, :through => :likes, :source => :user
+  belongs_to :user
+
+end
