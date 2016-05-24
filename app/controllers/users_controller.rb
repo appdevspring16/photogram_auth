5d159a2f3f5ff079ab51f3459d8f348e1709e825
shao313 <shaozhu@chicagobooth.edu>
2016-05-23 21:06:22 -0500

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    end
-  end 
+  end
+
+  def show
+    @user = User.find_by({:id => params[:user_id]})
+    @photos = @user.photos
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -55,10 +55,9 @@ Rails.application.routes.draw do
   #------------------------------
 
   # Create user index
-  get "/users", :controller => "users", :action => "index"
-
-
   devise_for :users
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:user_id", :controller => "users", :action => "show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
