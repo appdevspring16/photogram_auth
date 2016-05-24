bc04e10a7be7f88d2a32ce6f5087ab9ca7af53e8
Leland Brewster <jlbrew3@users.noreply.github.com>
2016-05-24 14:54:51 -0500

    latest changes including username auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
-  
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
+  end
+
 end
