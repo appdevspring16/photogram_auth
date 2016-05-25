ed11ca323659bf94b4202af5f64070ce8c8defe9
Dan Tuller <dtuller@gmail.com>
2016-05-25 13:29:46 -0500

    dan-tuller-photogram_auth-finished
    Everything complete, including the optional Carrierwave functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+
+    devise_parameter_sanitizer.for(:account_update) << :username
+  end
+
 end
