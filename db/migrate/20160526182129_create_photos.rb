6df28ec7a85ba747a7289d78dc29dcdf7a62d4a1
Brian Chingono <chingono@chicagobooth.edu>
2016-05-26 13:28:41 -0500

    Added models

---
+++ db/migrate/20160526182129_create_photos.rb
@@ -0,0 +1,17 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+
+      t.text :caption
+
+      t.string :image
+
+      t.integer :user_id
+
+
+      t.timestamps
+
+    end
+
+  end
+end
