1037b17a18f72da654a5b461e3f5285b3cfa948f
Matthew Sidley <msidley@chicagobooth.edu>
2016-05-27 12:51:57 -0500

    done with 15

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,8 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
+
+  get "/users/:id", :controller => "users", :action => "show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
