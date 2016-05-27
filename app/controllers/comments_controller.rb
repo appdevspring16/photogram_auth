772ae8a90c436077135565ed2275fe29a65ddbb9
alathans <alathans@users.noreply.github.com>
2016-05-27 17:03:27 -0500

    up to step 5 - now need to add associations

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,52 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new
+    @comment.photo_id = params[:photo_id]
+    @comment.body = params[:body]
+    @comment.user_id = params[:user_id]
+
+    if @comment.save
+      redirect_to "/comments", :notice => "Comment created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    @comment.photo_id = params[:photo_id]
+    @comment.body = params[:body]
+    @comment.user_id = params[:user_id]
+
+    if @comment.save
+      redirect_to "/comments", :notice => "Comment updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    @comment.destroy
+
+    redirect_to "/comments", :notice => "Comment deleted."
+  end
+end
