2a51a57159ef02693fb5c603aca09c906514e949
Dan Tuller <dtuller@gmail.com>
2016-05-27 08:57:37 -0500

    Revert "dan-tuller-photogram_auth-finished"
    This reverts commit ed11ca323659bf94b4202af5f64070ce8c8defe9.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,4 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-
-    devise_parameter_sanitizer.for(:account_update) << :username
-  end
-
 end
