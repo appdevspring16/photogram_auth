cbe05514653db8dd7fd2af803dd4814ef5e7a472
Cristina Costa <cncosta25@gmail.com>
2016-05-28 15:47:03 -0500

    step 13
    current_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, :presence => true, :uniqueness => true
+
+  has_many :photos
+  has_many :comments
+  has_many :likes
+  has_many :liked_photos, :through => :likes, :source => :photo
+
 end
