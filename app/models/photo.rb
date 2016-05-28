0c1cd85b9eea11ada517723113e6063ee2907e0b
vparikh0 <vparikh0@chicagobooth.edu>
2016-05-27 21:20:23 -0500

    vrajesh_step19_part2

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,5 +7,6 @@ class Photo < ActiveRecord::Base
   has_many :liked_photos, :through => :likes, :source => :photo
 
   # has_many :likes, :class_name => "Like", :foreign_key => "photo_id"
-  has_many :comments, :class_name => "Comment", :foreign_key => "photo_id"
+  has_many :comments
+
 end
