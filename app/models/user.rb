a432d3727c24043ce88cd928f062439e28250d1f
Rosa Rodriguez <rrodrig1@users.noreply.github.com>
2016-05-28 16:46:45 -0500

    Added Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, :presence => true, :uniqueness => true
-  
+  has_many :photos
+  has_many :comments
+  has_many :likes
+  has_many :liked_photos, :through => :likes, :source => :photo
 end
