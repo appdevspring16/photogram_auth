018615510302fa2f04f508c2a53148510d88cdc0
Rosa Rodriguez <rrodrig1@users.noreply.github.com>
2016-05-28 16:36:52 -0500

    Added validations to the tables

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,5 @@
 class Like < ActiveRecord::Base
+  validates :user_id, :presence => true, uniqueness => scope {:scope => :photo_id}
+  validates :photo_id, :presence => true
+
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,4 @@
 class Photo < ActiveRecord::Base
+  validates :user_id, :presence => true
+
 end
