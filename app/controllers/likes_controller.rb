3716c3b5bef857ec5fe3257fbeecc17d09b90577
Maayan <maharon@>
2016-05-28 18:09:17 -0500

    MA_initial

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,48 @@
+class LikesController < ApplicationController
+  def index
+    @likes = Like.all
+  end
+
+  def show
+    @like = Like.find(params[:id])
+  end
+
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @like = Like.new
+    @like.user_id = params[:user_id]
+    @like.photo_id = params[:photo_id]
+
+    @like.save
+    redirect_to "/photos", :notice => "Like created successfully."
+
+  end
+
+  def edit
+    @like = Like.find(params[:id])
+  end
+
+  def update
+    @like = Like.find(params[:id])
+
+    @like.user_id = params[:user_id]
+    @like.photo_id = params[:photo_id]
+
+    if @like.save
+      redirect_to "/likes", :notice => "Like updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+
+    @like.destroy
+
+    redirect_to "/photos", :notice => "Like deleted."
+  end
+end
