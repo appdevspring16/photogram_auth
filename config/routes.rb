144b28b1f2bf411432b166e11fd09b323ca91429
Andrea Ramirez <aramirez@users.noreply.github.com>
2016-05-29 16:07:20 -0500

    username field

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,10 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
+
+  # users index
+  get "/users", :controller => "users", :action => "index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
