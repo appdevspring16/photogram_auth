d0105cb3b15ee3303779df7de46cd94324db62d9
hkerstens <h.kerstens@chicagobooth.edu>
2016-05-31 12:37:08 -0500

    Everything but 20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @likes = Like.where({:user_id => @user.id})
   end
+
 
   def likes
     @user = User.find(current_user[:id])
-    end
+  end
 end
