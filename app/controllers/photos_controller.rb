66c0c96dbaf3f7fa6d29df0bcc5104465157c342
SamanthaSet <sset@chicagobooth.edu>
2016-05-31 18:18:51 -0400

    Almost everything except likes

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,55 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+    @likes = Like.all
+    @like = Like.new
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+    @photo = Photo.new
+    @photo.user_id = current_user
+  end
+
+  def create
+    @photo = Photo.new
+    @photo.caption = params[:caption]
+    @photo.image = params[:image]
+    @photo.user_id = params[:user_id]
+
+    if @photo.save
+      redirect_to "/photos", :notice => "Photo created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    @photo.caption = params[:caption]
+    @photo.image = params[:image]
+    @photo.user_id = params[:user_id]
+
+    if @photo.save
+      redirect_to "/photos", :notice => "Photo updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+
+    @photo.destroy
+
+    redirect_to "/photos", :notice => "Photo deleted."
+  end
+end
