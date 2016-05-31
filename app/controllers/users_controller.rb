c91f221b2dfc84c6ea38c7ff048c120aed38100d
hkerstens <h.kerstens@chicagobooth.edu>
2016-05-31 14:09:29 -0500

    Included heart, still need to copy to all others

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @photo = Photo.where({:user_id => @user.id})
     @likes = Like.where({:user_id => @user.id})
   end
 
