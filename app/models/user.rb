3e4169c6de5425889b5dea13cf0943ae576a70b3
dangrana <danmgrana@users.noreply.github.com>
2016-05-31 23:49:09 -0500

    tues1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
 
   validates :username, :presence => true, :uniqueness => true
 
+  has_many :photos
+  has_many :comments
+  has_many :likes
+
   has_many :liked_photos, :through => :likes, :source => :photo
 
 end
