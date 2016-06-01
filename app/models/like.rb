82a494741d1537e93f1f2ee6d71d7fa3be933ba4
Suresh Govindaraj <g123sk@gmail.com>
2016-06-01 00:51:55 -0500

    user login signin signout

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,12 @@
 class Like < ActiveRecord::Base
   # Like
   # user_id: presence, uniqueness in combination with photo
-  validates :user_id, :presence => true, uniqueness => { :scope => :photo_id }
+  validates :user_id, :presence => true, :uniqueness => { :scope => :photo_id }
   # photo_id: presence
   validates :photo_id, :presence => true
+
+  # association
+  belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
+  belongs_to(:photo, :class_name => "Photo", :foreign_key => "photo_id")
+
 end
