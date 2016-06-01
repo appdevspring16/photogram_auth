f22a5b6a7c68113c1f7178c1619ca4f645f5478a
Suresh Govindaraj <g123sk@gmail.com>
2016-06-01 00:46:00 -0500

    associations in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,5 +8,8 @@ class Comment < ActiveRecord::Base
   # body: presence
   validates :body, :presence => true
 
+  # association
+  belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
+  belongs_to(:photo, :class_name => "Photo", :foreign_key => "photo_id")
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ActiveRecord::Base
 
   # User
   # username: presence,  true
-    validates :username, :presence => true, :uniqueness => true
+  validates :username, :presence => true, :uniqueness => true
+
+  # association
+  has_many( :photos, :class_name => "Photo", :foreign_key => "user_id")
+  has_many( :comments, :class_name => "Comment", :foreign_key => "user_id")
+  has_many( :likes, :class_name => "Like", :foreign_key => "user_id")
+
+  has_many( :liked_photos, :through => :likes, :source => :photo)
+
 end
