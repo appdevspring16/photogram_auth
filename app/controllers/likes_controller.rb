4faa02441a0c10a632e973e02c931517fd5649c1
Aaron Goltz <aarongoltz@users.noreply.github.com>
2016-06-02 00:57:11 -0500

    Have to finish the quick add comments.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -17,7 +17,7 @@ class LikesController < ApplicationController
     @like.photo_id = params[:photo_id]
 
     if @like.save
-      redirect_to "/likes", :notice => "Like created successfully."
+      redirect_to "/photos", :notice => "Like created successfully."
     else
       render 'new'
     end
@@ -45,6 +45,6 @@ class LikesController < ApplicationController
 
     @like.destroy
 
-    redirect_to "/likes", :notice => "Like deleted."
+    redirect_to "/photos", :notice => "Like deleted."
   end
 end
