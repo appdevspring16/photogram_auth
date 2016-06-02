2e6a398e78f5236b7c789dd6446a890fd5a1198c
Rosa Rodriguez <rrodrig1@users.noreply.github.com>
2016-06-01 21:18:18 -0500

    Added RCAV for User Photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def show
+    @user = User.find(params[:id])
+    @photos = current_user.photos
+  end
 end
