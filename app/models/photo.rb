e4494e1ded4b86a70f589bfc8db89b14725c0028
deepeshg <deepesh.goyal@gmail.com>
2016-06-01 21:57:06 -0400

    Step 11

---
+++ app/models/photo.rb
@@ -0,0 +1,7 @@
+class Photo < ActiveRecord::Base
+  validates :user_id, presence: true
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+  has_many :fans, :through => :likes, :source => :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  has_many :comments
+  has_many :photos
+  has_many :likes
+  has_many :liked_photos, :through => :likes, :source => :photo
+end
