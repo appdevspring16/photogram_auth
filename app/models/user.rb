32a6fd773e1017c396dc081c4d030bd641769478
Leland Brewster <jlbrew3@users.noreply.github.com>
2016-06-03 14:23:37 -0500

    destroy dependents

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :username, :presence => true, :uniqueness => true
 
   has_many :photos
-  has_many :comments
-  has_many :likes
+  has_many :comments, :dependent => :destroy
+  has_many :likes, :dependent => :destroy
   has_many :liked_photos, :through => :likes, :source => :photo
 end
