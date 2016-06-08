2b8267a444474d14dc7eb73e8c40be75923af93c
Yelena Kulensova <helen.erikson@users.noreply.github.com>
2016-06-08 18:38:58 +0200

    created table Like
    rails generate starter:resource like user_id:integer photo_id:integer
    
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608163557) do
+ActiveRecord::Schema.define(version: 20160608163742) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "photo_id"
     t.text     "body"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "photo_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
