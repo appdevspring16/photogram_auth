e408bb1709564ccb0f7c96827671782eb8de7687
Yelena Kulensova <helen.erikson@users.noreply.github.com>
2016-06-08 18:33:59 +0200

    created Photo table
    rails generate starter:resource photo caption:text image:string
    user_id:integer
    
    rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Photo resource:
+  # CREATE
+  get "/photos/new", :controller => "photos", :action => "new"
+  post "/create_photo", :controller => "photos", :action => "create"
+
+  # READ
+  get "/photos", :controller => "photos", :action => "index"
+  get "/photos/:id", :controller => "photos", :action => "show"
+
+  # UPDATE
+  get "/photos/:id/edit", :controller => "photos", :action => "edit"
+  post "/update_photo/:id", :controller => "photos", :action => "update"
+
+  # DELETE
+  get "/delete_photo/:id", :controller => "photos", :action => "destroy"
+  #------------------------------
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608162808) do
+ActiveRecord::Schema.define(version: 20160608163204) do
+
+  create_table "photos", force: :cascade do |t|
+    t.text     "caption"
+    t.string   "image"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
