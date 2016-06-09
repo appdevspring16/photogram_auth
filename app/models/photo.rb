18821f579b1d77b93a0605a53cb523531a76f094
Pedro A. Diaz de Leon <pantonio3@users.noreply.github.com>
2016-06-08 21:57:40 -0500

    Likes fixed
    Likes fixed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,5 +4,7 @@ class Photo < ActiveRecord::Base
 has_many :comments, :class_name => "Comment", :foreign_key => "photo_id"
 has_many :likes
 belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+has_many :fans, :through => :likes, :source => :user
+
 
 end
